a52bbf78d31045c15debb44f42e2fe6cab6986e3
Marcos Rosales <gitmarcos@correo.com>
2024-05-04 19:04:58 -0500

    ...

--- js/landing-function.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var toggler = document.querySelector('.custom-toggler.navbar-toggler');
-    var navbarCollapse = document.querySelector('.navbar-collapse');
-
-    toggler.addEventListener('click', function() {
-        navbarCollapse.classList.toggle('show');
-    });
-});
